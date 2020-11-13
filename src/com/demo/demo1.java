634f7b2abe0d2e61a45807e410f837bf04a3e2d7
sourabhprasad11 <sourabh.prasad444@gmail.com>
2020-11-13 09:02:23 +0530

    Added: Structure of the of Addressbook

---
+++ src/com/demo/demo1.java
@@ -0,0 +1,60 @@
+package com.demo;
+
+import java.util.Scanner;
+
+public class demo1 {
+
+	public static void main(String[] args) {
+int ans;
+		
+		
+		System.out.println("1. New AddressBook");
+		System.out.println("2. Open AddressBook");
+		System.out.println("3. Save AddressBook");
+		System.out.println("4. SaveAs AddressBook");
+		System.out.println("5. Close AddressBook");
+		System.out.println("0. Quit");
+		
+		Scanner ip=new Scanner(System.in);
+		System.out.println("Enter the number from above: ");
+		ans=ip.nextInt();
+		do {
+		switch(ans) {
+			case 1: 
+					//abi.newAddressBook();
+					System.out.println("working");
+					break;
+			case 2: 
+					//abi.openAddressBook();
+					System.out.println("working");
+				break;
+			case 3: 
+					//abi.saveAddressBook();
+					System.out.println("working");
+				break;
+			case 4: 
+					//abi.saveasAddressBook();
+					System.out.println("working");
+				break;
+			case 5: 
+					//abi.closeAddressBook();
+					System.out.println("working");
+				break;
+			case 6: 
+					//abi.quit();
+					System.out.println("working");
+				break;
+			default:
+					System.out.println("You have entered the wrong number...Terminating the application");
+					ans=0;
+				break;
+			}
+		}while(ans!=0);
+			
+		
+
+
+
+	}
+
+}
